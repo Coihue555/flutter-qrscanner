63a4debdc6465a3ed14e1636d630a42f4b93dc7d
Coihue555 <kevlar555@gmail.com>
2022-03-09 10:32:59 -0300

    instalacion de qrscanner

---
+++ flutter_qrscanner/lib/pages/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qrscanner/pages/direcciones_pages.dart';
+import 'package:flutter_qrscanner/pages/mapas_page.dart';
+import 'package:flutter_qrscanner/providers/ui_provider.dart';
+import 'package:flutter_qrscanner/widgets/custom_navigatorbar.dart';
+import 'package:flutter_qrscanner/widgets/scan_button.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+   
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Historial'),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.delete_forever)
+          )
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch(currentIndex){
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+      
+    }
+  }
+}
---
+++ flutter_qrscanner/lib/pages/mapa_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MapaPage extends StatelessWidget {
+   
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Localizacion'),
+      ),
+      body: const Center(
+         child: Text('MapaPage'),
+      ),
+    );
+  }
+}
